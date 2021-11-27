327eedc0c6694a9540189a26073adf456709517d
samupatu004 <94621869+samupatu004@users.noreply.github.com>
2021-11-27 18:36:49 +0100

    from () to =

--- progetti/stazioni-radio/index.js
+++ progetti/stazioni-radio/index.js
@@ -186,7 +186,7 @@ function handleNearObject(entity, distance) {
         if (distance < MIN_DISTANCE) {
             // TODO mostrare qualcosa
             console.log('ce un luogo molto vicino, < N');
-            document.querySelector('.pdf_opener').style.diplay('block');
+            document.querySelector('.pdf_opener').style.diplay = 'block';
             // settiamo l'entity più vicina, che verrà rimossa quando la stessa entity sarà
             // ad una distanza >  MIN_DISTANCE
             closestEntity = entity;
@@ -206,7 +206,7 @@ function handleNearObject(entity, distance) {
         // c'è già, è quella corrente ma ora si trova più distante di MIN_DISTANCE
         if (closestEntity === entity && distance >= MIN_DISTANCE) {
             // TODO rimuovo quello attuale
-            document.querySelector('.pdf_opener').style('none');
+            document.querySelector('.pdf_opener').style = 'none';
             closestEntity = null;
         }
     }
