57d43b8b0cb0f38ba9e998787ad4748dcbd90c13
samupatu004 <94621869+samupatu004@users.noreply.github.com>
2021-11-27 18:35:26 +0100

    desperate fix (found "." missing)

--- progetti/stazioni-radio/index.js
+++ progetti/stazioni-radio/index.js
@@ -181,13 +181,12 @@ function elaboratePlaces(places) {
 }
 
 function handleNearObject(entity, distance) {
-    var bottone = document.getElementsByClassName('pdf_opener').style;
     // se non c'è già una entity sotto MIN_DISTANCE...
     if (!closestEntity) {
         if (distance < MIN_DISTANCE) {
             // TODO mostrare qualcosa
             console.log('ce un luogo molto vicino, < N');
-            document.getElementsById('pdf_opener').style.diplay: 'block';
+            document.querySelector('.pdf_opener').style.diplay('block');
             // settiamo l'entity più vicina, che verrà rimossa quando la stessa entity sarà
             // ad una distanza >  MIN_DISTANCE
             closestEntity = entity;
@@ -207,7 +206,7 @@ function handleNearObject(entity, distance) {
         // c'è già, è quella corrente ma ora si trova più distante di MIN_DISTANCE
         if (closestEntity === entity && distance >= MIN_DISTANCE) {
             // TODO rimuovo quello attuale
-            document.getElementsById('pdf_opener').style: 'none';
+            document.querySelector('.pdf_opener').style('none');
             closestEntity = null;
         }
     }
