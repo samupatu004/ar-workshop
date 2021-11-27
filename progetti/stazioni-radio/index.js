19cd89d419164e2734e86d20569d20773549b9c6
samupatu004 <94621869+samupatu004@users.noreply.github.com>
2021-11-27 17:20:56 +0100

    removed duplicated detailAdress

--- progetti/stazioni-radio/index.js
+++ progetti/stazioni-radio/index.js
@@ -62,11 +62,6 @@ window.onload = () => {
     window.detailTitle = document.querySelector('.detail-title');
     window.detailDistanceElem = document.querySelector('.detail-distance');
     window.detailAddressElem = document.querySelector('.detail-address');
-    window.detailDistanceElem = document.querySelector('.detail-distance');
-    window.detailAddressElem = document.querySelector('.detail-address');
-    window.detailDistanceElem = document.querySelector('.detail-distance');
-    window.detailAddressElem = document.querySelector('.detail-address');
-    window.moreDetailsLink = document.querySelector('.more-details');
 
     // every TIME_TO_UPDATE seconds, fetch other markers
     setInterval(() => {
